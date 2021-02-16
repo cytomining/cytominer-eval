b7599e464000e7c76f817b9925579e6f5a57eef2
gwaygenomics <gregory.way@gmail.com>
2021-02-16 17:14:18 -0500

    fix typo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages
 setup(
     name="cytominer_eval",
     description="Methods to evaluate profiling dataframes with features and metadata",
-    long_description="Profiling experiments result in a profile, or fingerprint, of a biomedical perturbation of cells. This package evaluates the fingeprint.",
+    long_description="Profiling experiments result in a profile, or fingerprint, of a biomedical perturbation of cells. This package evaluates the fingerprint.",
     maintainer="Gregory Way",
     maintainer_email="gregory.way@gmail.com",
     url="https://github.com/cytomining/cytominer-eval",
