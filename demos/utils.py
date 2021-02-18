4ed796d87643f3f1c36ae06764cd0fcbd5795cff
gwaygenomics <gregory.way@gmail.com>
2021-02-17 21:29:40 -0500

    add cell painting demo

---
+++ demos/utils.py
@@ -0,0 +1,11 @@
+import pathlib
+import pandas as pd
+
+
+def load_cell_health(commit="7d63d4a43014c757fd0c77c0fd1c19540f17cc3d"):
+    base_url = f"https://github.com/broadinstitute/cell-health/raw/{commit}"
+
+    url = f"{base_url}/1.generate-profiles/data/processed/cell_health_profiles_merged.tsv.gz"
+    df = pd.read_csv(url, sep="\t")
+    
+    return df
