23ebfffc7440edf427b5fe0526c0040ab198c787
gwaygenomics <gregory.way@gmail.com>
2021-02-17 21:32:20 -0500

    add new line

--- demos/utils.py
+++ demos/utils.py
@@ -7,5 +7,5 @@ def load_cell_health(commit="7d63d4a43014c757fd0c77c0fd1c19540f17cc3d"):
 
     url = f"{base_url}/1.generate-profiles/data/processed/cell_health_profiles_merged.tsv.gz"
     df = pd.read_csv(url, sep="\t")
-    
+
     return df
