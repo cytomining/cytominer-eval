1cd56d36098469b89a5d033128f640624e20a5d6
michaelbornholdt <michael.bornholdt@outlook.com>
2021-04-21 13:42:55 -0400

    add test

--- cytominer_eval/tests/test_operations/test_enrichment.py
+++ cytominer_eval/tests/test_operations/test_enrichment.py
@@ -1,5 +1,66 @@
-import cytominer_eval.operations.enrichment
+import os
+import random
+import pytest
+import pathlib
+import tempfile
+import numpy as np
+import pandas as pd
+
+# path = os.getcwd()
+# print(path)
+# import sys
+# sys.path.insert(0, "../../")
+# print(os.environ['PYTHONPATH'])
 
 
+from cytominer_eval.transform import metric_melt
+from cytominer_eval.operations.enrichment import enrichment
+
+from pycytominer.cyto_utils import infer_cp_features
+
+random.seed(3141)
+tmpdir = tempfile.gettempdir()
+
+
+# Load LINCS dataset
+example_file = "SQ00015054_normalized_feature_select.csv.gz"
+example_file = pathlib.Path(
+    "{file}/../../example_data/compound/{eg}".format(
+        file=os.path.dirname(__file__), eg=example_file
+    )
+)
+
+df = pd.read_csv(example_file)
+
+features = infer_cp_features(df)
+meta_features = infer_cp_features(df, metadata=True)
+
+replicate_groups = ["Metadata_broad_sample"]
+
+similarity_melted_df = metric_melt(
+    df=df,
+    features=features,
+    metadata_features=meta_features,
+    similarity_metric="pearson",
+    eval_metric="enrichment",
+)
 def test_enrichment():
-    assert True
+    result = []
+    for p in np.arange(1, 0.97, -0.005):
+        r = enrichment(
+            similarity_melted_df=similarity_melted_df,
+            replicate_groups=replicate_groups,
+            percentile=p,
+        )
+        result.append(r)
+    result_df = pd.DataFrame(result)
+
+    # check for correct shape and starts with 1.0
+    assert result_df.shape == (7, 4)
+    assert result_df.percentile[0] == 1.0
+    # check if the higher percentiles are larger than the small one
+    assert result_df.percentile[1] > result_df.percentile.iloc[-1]
+
+
+
+#test_enrichment()
