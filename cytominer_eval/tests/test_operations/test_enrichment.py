d27ecfa878481050839d2dbb3a658b3765648562
michaelbornholdt <michael.bornholdt@outlook.com>
2021-04-21 16:58:31 -0400

    add comparisson test

--- cytominer_eval/tests/test_operations/test_enrichment.py
+++ cytominer_eval/tests/test_operations/test_enrichment.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 from cytominer_eval.transform import metric_melt
 from cytominer_eval.operations.enrichment import enrichment
+from cytominer_eval import evaluate
 
 
 random.seed(3141)
@@ -56,3 +57,22 @@ def test_enrichment():
     assert result_df.percentile[0] == 1.0
     # check if the higher percentiles are larger than the small one
     assert result_df.percentile[1] > result_df.percentile.iloc[-1]
+
+
+def test_compare_functions():
+    percentile = 0.9
+    eval_res = evaluate(
+        profiles=df,
+        features=features,
+        meta_features=meta_features,
+        replicate_groups=replicate_groups,
+        operation="enrichment",
+        similarity_metric="pearson",
+        enrichment_percentile=percentile,
+    )
+    enr_res = enrichment(
+        similarity_melted_df=similarity_melted_df,
+        replicate_groups=replicate_groups,
+        percentile=percentile,
+    )
+    assert enr_res == eval_res
