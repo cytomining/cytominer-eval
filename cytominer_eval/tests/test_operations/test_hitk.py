71010bc9c1ddba6bb06f6f3cfa99ec049ff8f605
michaelbornholdt <michael.bornholdt@outlook.com>
2021-10-06 15:23:41 -0400

    Added "all" to percent scores

--- cytominer_eval/tests/test_operations/test_hitk.py
+++ cytominer_eval/tests/test_operations/test_hitk.py
@@ -7,12 +7,11 @@ import numpy as np
 import pandas as pd
 
 import sys
+
 sys.path.insert(0, "/Users/mbornhol/git/mycyto/cytominer-eval")
 
 from cytominer_eval.transform import metric_melt
 from cytominer_eval.operations import hitk
-
-
 
 
 random.seed(42)
@@ -27,7 +26,7 @@ example_file = pathlib.Path(
 )
 
 df = pd.read_csv(example_file)
-df['Metadata_moa'] = df['Metadata_moa'].fillna('unknown')
+df["Metadata_moa"] = df["Metadata_moa"].fillna("unknown")
 
 
 meta_features = [
@@ -43,13 +42,9 @@ similarity_melted_df = metric_melt(
     eval_metric="hitk",
 )
 
-percent_list = [2,5,10,100]
+percent_list = [2, 5, 10, 100]
 indexes, percent_results = hitk(similarity_melted_df, percent_list)
 
-def test_hitk():
-    assert percent_results == {2: 690, 5: 984, 10: 1147, 100: 0}
-    assert max(indexes) == 382
-    assert min(indexes) == 0
 
 def test_number_of_hits():
     """Calculates the number of indexes based off the MOA value count in the original df
@@ -58,8 +53,26 @@ def test_number_of_hits():
     assert s == len(indexes)
 
 
+def test_hitk_list():
+    assert percent_results == {2: 690, 5: 984, 10: 1147, 100: 0}
+    assert max(indexes) == 382
+    assert min(indexes) == 0
+
+
+percent_all = "all"
+indexes_all, percent_results_all = hitk(similarity_melted_df, percent_all)
+
+
+def test_hitk_all():
+    assert indexes == indexes_all
+    assert percent_results_all[0] == 150.75
+    assert percent_results_all[383] == 0.0
+
+
 ran = df.copy()
-ran[features] = ran[features].iloc[np.random.permutation(len(df))].reset_index(drop=True)
+ran[features] = (
+    ran[features].iloc[np.random.permutation(len(df))].reset_index(drop=True)
+)
 similarity_melted_ran = metric_melt(
     df=ran,
     features=features,
@@ -69,18 +82,11 @@ similarity_melted_ran = metric_melt(
 )
 ran_indexes, ran_percent_results = hitk(similarity_melted_ran, percent_list)
 
+
 def test_random_input():
     len(ran_indexes) == len(indexes)
     assert ran_percent_results[100] == 0
-    median_index = abs(np.median(ran_indexes) - len(similarity_melted_ran.pair_b_index.unique()) / 2)
+    median_index = abs(
+        np.median(ran_indexes) - len(similarity_melted_ran.pair_b_index.unique()) / 2
+    )
     assert median_index < 30
-
-
-
-
-
-test_hitk()
-
-test_number_of_hits()
-
-test_random_input()
--- cytominer_eval/utils/hitk_utils.py
+++ cytominer_eval/utils/hitk_utils.py
@@ -1,5 +1,3 @@
-
-
 def index_hits(df):
     """Adds the rank or index of each connection to the dataframe.
     It then further drops and reorders unnecessary columns.
@@ -13,46 +11,70 @@ def index_hits(df):
     slimmed dataframe with rank and same_moa
 
     """
-    df = df.sort_values(['similarity_metric'], ascending=False) # inplace=True,
     # rank all compounds by their similarity
-    df['rank'] = range(len(df))
+    df = df.sort_values(["similarity_metric"], ascending=False)
+    df["rank"] = range(len(df))
 
     # add a column which is true if pair a and b have the same MOA
     moa = df.Metadata_moa_pair_a.iloc[0]
-    df['same_moa'] = df['Metadata_moa_pair_b'] == moa
+    df["same_moa"] = df["Metadata_moa_pair_b"] == moa
 
     # clean and reorder columns
-    df = df[['Metadata_broad_sample_pair_a', 'Metadata_moa_pair_a', 'Metadata_broad_sample_pair_b',
-             'Metadata_moa_pair_b', 'rank', 'same_moa']]
-    df = df.reset_index(drop=True) #inplace=True
+    df = df[
+        [
+            "Metadata_broad_sample_pair_a",
+            "Metadata_moa_pair_a",
+            "Metadata_broad_sample_pair_b",
+            "Metadata_moa_pair_b",
+            "rank",
+            "same_moa",
+        ]
+    ]
+    df = df.reset_index(drop=True)
     return df
 
 
 def percentage_scores(similarity_melted_df, indexes, p_list):
-    """
+    """Calculates the number of indexes below a certain percentage.
+    All of the scores are cleaned with the expected random distribution.
+    If p_list = "all" then, instead of percentages, all classes are enumerated and indexes counted.
 
     Parameters
     ----------
-    similarity_melted_df :
-    p_list :
-    indexes :
+    similarity_melted_df : pandas.DataFrame
+        An elongated symmetrical matrix indicating pairwise correlations between
+        samples
+    indexes : list
+        long list of all MOA hit indexes
+    p_list : list or 'all'
+        list of percentages to score
 
     Returns
     -------
-
+    d : dict
+        dictionary with percentages and scores or a full list of indexes and scores
     """
     # get the number of compounds in this dataset
     nr_of_classes = len(similarity_melted_df.pair_b_index.unique())
-
     d = {}
     total_hits = len(indexes)
 
-    # calculate the accumalated hits at different percentages
-    for p in p_list:
-        # calculate the integers referring to the percentage of the full range of classes
-        p_int = int(p * nr_of_classes / 100)
-        # calculate the hits that are expected for a random distribution
-        expected_hits = int(p * total_hits / 100)
-        d[p] = len([i for i in indexes if i <= p_int]) - expected_hits
+    if p_list == "all":
+        # diff is the accumulated difference between the index hits and the average bins
+        diff = 0
+        average_bin = total_hits / nr_of_classes
+        for n in range(nr_of_classes):
+            hits_n = indexes.count(n)
+            diff += hits_n - average_bin
+            d[n] = diff
+
+    else:
+        # calculate the accumulated hits at different percentages
+        for p in p_list:
+            # calculate the integers referring to the percentage of the full range of classes
+            p_int = int(p * nr_of_classes / 100)
+            # calculate the hits that are expected for a random distribution
+            expected_hits = int(p * total_hits / 100)
+            d[p] = len([i for i in indexes if i <= p_int]) - expected_hits
 
     return d
